cb7b36819c2c36516d102c8cc059740dff196a52
Ismoiljon <hs.umarov21@gmail.com>
2024-09-08 16:00:20 +0500

    push 13

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -5,12 +5,24 @@
     <a href="{{ route('sales.create') }}">Add Sale</a>
     <ul>
         @foreach($sales as $sale)
-            <li>{{ $sale->client->name }} bought {{ $sale->quantity }} {{ $sale->product->name }} for ${{ $sale->total_price }}
-                <form action="{{ route('sales.destroy', $sale->id) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">Delete</button>
-                </form>
+            <li>
+                {{ $sale->client->name }} bought {{ $sale->quantity }} {{ $sale->product->name }} for ${{ $sale->total_price }}
+                
+                @if($sale->status == 'refunded')
+                    <span style="color: red;">(Refunded)</span>
+                @elseif($sale->status == 'cancelled')
+                    <span style="color: orange;">(Cancelled)</span>
+                @endif
+
+                @if($sale->status == 'completed')
+                    <form action="{{ route('sales.destroy', $sale->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">Delete</button>
+                    </form>
+                @else
+                    <span style="color: gray;">(Cannot delete)</span>
+                @endif
             </li>
         @endforeach
     </ul>
