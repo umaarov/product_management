61bb24f9b0e92f1d434d751a8a21fda6ad56042d
Ismoiljon <hs.umarov21@gmail.com>
2024-09-13 17:18:04 +0500

    push 27

--- resources/views/sales/create.blade.php
+++ resources/views/sales/create.blade.php
@@ -1,19 +1,20 @@
 @extends('layouts.app')
-
+@vite('resources/css/sales/sale.css')
 @section('content')
     <h1>Create Sale</h1>
     <form action="{{ route('sales.store') }}" method="POST">
         @csrf
+
         <label for="client_id">Client:</label>
         <select name="client_id" id="client_id">
-            @foreach($clients as $client)
+            @foreach ($clients as $client)
                 <option value="{{ $client->id }}">{{ $client->name }}</option>
             @endforeach
         </select>
 
         <label for="product_id">Product:</label>
         <select name="product_id" id="product_id">
-            @foreach($products as $product)
+            @foreach ($products as $product)
                 <option value="{{ $product->id }}">{{ $product->name }}</option>
             @endforeach
         </select>
--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -1,16 +1,32 @@
 @extends('layouts.app')
-
+@vite('resources/css/sales/sale.css')
 @section('content')
     <h1>Sales</h1>
-    <a href="{{ route('sales.create') }}">Add Sale</a>
-    <ul>
-        @foreach($sales as $sale)
-            <li>
-                {{ $sale->client->name }} bought {{ $sale->quantity }} {{ $sale->product->name }} for ${{ $sale->total_price }}
-                @if($sale->status === 'refunded')
-                    <span>(Refunded)</span>
-                @endif
-            </li>
-        @endforeach
-    </ul>
+    <a href="{{ route('sales.create') }}" class="add-sale">Add Sale</a>
+    <table>
+        <thead>
+            <tr>
+                <th>Client</th>
+                <th>Product</th>
+                <th>Quantity</th>
+                <th>Total Price</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($sales as $sale)
+                <tr>
+                    <td>{{ $sale->client->name }}</td>
+                    <td>{{ $sale->product->name }}</td>
+                    <td>{{ $sale->quantity }}</td>
+                    <td>${{ $sale->total_price }}</td>
+                    <td>
+                        @if ($sale->status === 'refunded')
+                            <span>(Refunded)</span>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
