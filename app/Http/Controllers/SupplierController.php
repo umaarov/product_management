31ba2e379dbc26988518bdd7df3d439acb4c7f7d
Ismoiljon <hs.umarov21@gmail.com>
2024-09-14 11:57:46 +0500

    push 41

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Supplier;
 use App\Models\Category;
-use App\Models\Product;
 use Illuminate\Http\Request;
 
 class SupplierController extends Controller
